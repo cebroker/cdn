b26f1a4d3917b02a1516dcf411d3407df2941973
Pedro Jimenez <pedroj.jimenezp@gmail.com>
2019-01-09 14:38:18 -0500

    Update install-oracle-instantclient-12-2.sh

--- oracle/install-oracle-instantclient-12-2.sh
+++ oracle/install-oracle-instantclient-12-2.sh
@@ -25,5 +25,3 @@ printf "Adding LD_LIBRARY_PATH to the PATH...\n"
 echo "export LD_LIBRARY_PATH=~/instantclient_12_2:$:LD_LIBRARY_PATH" >> ~/.bashrc
 
 printf "instantclient version 12.2 installed successfully\n\n"
-
-source ~/.bashrc
