ad9d356b41239875495c5319732026a030d31cd5
Pedro Jimenez <pedroj.jimenezp@gmail.com>
2019-01-09 14:36:06 -0500

    Improve bash

--- oracle/install-oracle-instantclient-12-2.sh
+++ oracle/install-oracle-instantclient-12-2.sh
@@ -1,6 +1,10 @@
 printf "Moving to home directory\n"
 cd ~
 
+printf "Installing script's dependencies\n"
+sudo yum install wget unzip
+
+
 printf "Downloading oracle 12.2\n"
 wget -O instantclient-basic-linux.x64-12.2.0.1.0.zip https://github.com/cebroker/cdn/blob/master/oracle/oracle-driver/instantclient-basic-linux.x64-12.2.0.1.0.zip?raw=true
 
@@ -13,7 +17,7 @@ ln -s libclntsh.so.12.1 libclntsh.so
 ln -s libocci.so.12.1 libocci.so
 cd ..
 
-printf "Instaling libaio dependency\n"
+printf "Installing libaio dependency\n"
 sudo yum install libaio
 
 
@@ -21,3 +25,5 @@ printf "Adding LD_LIBRARY_PATH to the PATH...\n"
 echo "export LD_LIBRARY_PATH=~/instantclient_12_2:$:LD_LIBRARY_PATH" >> ~/.bashrc
 
 printf "instantclient version 12.2 installed successfully\n\n"
+
+source ~/.bashrc
